d827920a16d5c6cfc8451c13347eb334774c4eb7
SP23 <phanchuan.23@gmail.com>
2018-08-21 13:01:55 +0700

    Giao dien

---
+++ app/src/main/java/com/example/sp23/baitaptuan2/Bai1Activity.java
@@ -0,0 +1,13 @@
+package com.example.sp23.baitaptuan2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bai1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai1);
+    }
+}
